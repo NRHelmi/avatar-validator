005841db626085683fa1eb8dc1a4c74bcb69b373
Nour Helmi <helmi.nour.ext@orange.com>
2024-01-15 21:04:32 +0100

    test bad sized avatar

--- tests/AvatarValidatorTests.py
+++ tests/AvatarValidatorTests.py
@@ -7,7 +7,12 @@ class TestAvatarValidator(unittest.TestCase):
     def test_validate_size(self):
         validator = AvatarValidator("./tests/data/right_sized_avatar.png")
         self.assertTrue(validator.validate_size())
+        self.assertTrue(validator.close())
+
+        validator = AvatarValidator("./tests/data/bad_sized_avatar.png")
+        self.assertFalse(validator.validate_size())
+        self.assertTrue(validator.close())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
